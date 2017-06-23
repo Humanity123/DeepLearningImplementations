de501c56c820b2764b3b51a08db0f624af76032b
Humanity123 <yashagrwl.iitkgp@gmail.com>
2017-06-23 11:10:31 +0530

    Added Cluster Class

--- SubEventDetection/sub_event.py
+++ SubEventDetection/sub_event.py
@@ -1,19 +1,12 @@
-import pickle
-import sklearn as 
-import numpy as np
 from math import *
 from nltk.tokenize import TweetTokenizer
-
-def clean_doc(doc):
-	tknzr = TweetTokenizer(reduce_len=True)
-	lmtzr = WordNetLemmatizer()
-	tokens = tknzr.tokenize(doc.decode('utf-8'))
-	cleaned_tokens = [lmtzr.lemmatize(token.lower(), pos='v') for token in tokens]
-	cleaned_doc=""
-	for token in cleaned_tokens: 
-		cleaned_doc += token + " "
-	return cleaned_doc.strip()
-
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle
+import csv
+import sklearn 
+import numpy as np
+import datetime
 
 def tf_idf( doc, term, list_of_docs):
 	num_docs_containing_term = 0.0
@@ -50,10 +43,98 @@ def cosine_similarity_score(vector1, vector2):
 	return sklearn.metrics.pairwise.cosine_similarity( np.array(vector1).reshape(1,-1), np.array(vector2).reshape(1,-1) )[0][0]
 
 
-def main():
+class Cluster:
+	'''class for a cluster'''
+	def __init__(self, feature, feature_index, similarity_metric, MBS_coeff, doc, doc_index):
+		''' feature_index is the index of the feature in the datapoints that would be processes'''
+		self.doc_list = [doc_index]
+		self.feature = feature
+		self.feature_index = feature_index
+		self.similarity_metric = similarity_metric
+		self.MBS = 1
+		self.MBS_prev = 0
+		self.MBS_coeff = MBS_coeff
+		self.centroid = doc[feature_index]
 
-if __name__=="__main__":
-	return main()
+	def similarity_score(self, doc):
+		return self.similarity_metric(self.centroid, doc[self.feature_index])
+
+	def get_threshold():
+		return self.MBS*self.MBS_coeff
+
+	def average_similarity(self, doc_list):
+		return sum([self.similarity_metric(self.centroid, doc_list[index][self.feature_index]) for index in self.doc_list]) / float(len(self.doc_list))
+
+	def update_MBS(self, doc_list, doc_index):
+		if len(self.doc_list <= 25 ):
+			self.MBS = average_similarity(doc_list)
+		elif self.MBS_prev >= 20:
+			self.MBS_prev = 0
+			self.MBS = average_similarity(doc_list)
+		else: 
+			self.MBS = (self.MBS * (len(self.doc_list) - 1) + self.similarity_metric(self.centroid, doc_list[doc_index][self.feature_index])) / float(len(self.doc_list))
+
+
+	def add_point(self, doc_list, doc_index):
+		self.doc_list.append(doc_index)
+		self.centroid = sum([ doc_list[index][self.feature_index] for index in self.doc_list])/len(self.doc_list)
+		self.update_MBS(doc_list, doc_index)
+
+
+
+
+
+
+class ClusterSolution:
+	'''class for cluster solution for a feature'''
+	def __init__(self, feature, feature_index, similarity_metric):
+		''' feature_index is the index of the feature in the datapoints that would be processes'''
+		self.cluster_list = []
+		self.feature = feature
+		self.feature_index = feature_index
+		self.similarity_metric = similarity_metric
+
+	
+
+
+def get_tfidf_scores(list_of_docs):
+	list_of_tweets = [ doc[0] for doc in list_of_docs ]
+	vectorizer = TfidfVectorizer(stop_words = 'english')
+	tfidf_score = np.array(vectorizer.fit_transform(list_of_tweets).todense())
+	tfidf_score_list = []
+	for index in range(tfidf_score.shape[0]):
+		tfidf_score_list.append(tfidf_score[index])
+	return tfidf_score_list
+
+def clean_doc(doc):
+	tknzr = TweetTokenizer(reduce_len=True)
+	lmtzr = WordNetLemmatizer()
+	tokens = tknzr.tokenize(doc.decode('utf-8'))
+	cleaned_tokens = [lmtzr.lemmatize(token.lower(), pos='v') for token in tokens]
+	cleaned_doc=""
+	for token in cleaned_tokens: 
+		cleaned_doc += token + " "
+	return cleaned_doc.strip()
+
+def create_docs(tweets):
+	'''Given File Containing Tweets returns lists of dics with each doc having the tweet text and days passed since epoch and tfidf scores'''
+	with open(tweets,'rb') as tweet_csv:
+		reader = csv.DictReader(tweet_csv)
+		tweet_text = []
+		tweet_days_since_epoch = []
+		for tweet in reader:
+			text = clean_doc(tweet['Tweet Text'])
+			days_since_epoch = ( datetime.datetime.strptime(tweet['Tweet Created At'],"%Y-%m-%d %H:%M:%S") - datetime.datetime(1970,1,1) ).days
+			tweet_text.append(text)
+			tweet_days_since_epoch.append(days_since_epoch)
+	tweet_tfidf_score_list = get_tfidf_scores(tweet_text)
+	return zip(tweet_text, tweet_days_since_epoch, tweet_tfidf_score_list)
+
+
+def main():
+	return 
+if __name__ == '__main__':
+    main()
 
 
 
