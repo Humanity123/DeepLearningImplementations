b7ad28fe6daf46b45f007911842e93a7ff07cc8e
Humanity123 <yashagrwl.iitkgp@gmail.com>
2017-06-25 00:45:06 +0530

    Refactoring Code

---
+++ SubEventDetection/similarity_score.py
@@ -0,0 +1,38 @@
+from math import *
+import sklearn 
+import numpy as np
+import datetime
+
+def tf_idf( doc, term, list_of_docs):
+	num_docs_containing_term = 0.0
+	for document in list_of_docs:
+		if term in document:
+			num_docs_containing_term += 1
+
+	if term in doc :
+		freq_term_in_doc = doc[term]
+	else :
+		freq_term_in_doc = 0
+
+	words_in_doc = 1.0 * sum(doc.values())
+	return (freq_term_in_doc / words_in_doc) * tf.log( len(list_of_docs) / num_docs_containing_term)
+
+def date_similarity_score(date1, date2):
+	''' if the difference in days is more than one month apart it is taken as 0'''
+	diff_in_days = abs(date1-date2)
+	seperation_limit_in_days = 30.0
+	if diff_in_days > seperation_limit_in_days:
+		return 0.0
+	return 1- ( diff_in_days / seperation_limit_in_days )
+
+def location_similarity_score(location1, location2):
+	''' similarity between two locations using the widely accepted Haversine Distance'''
+	longitude_1, latitude_1, longitude_2, latitude_2 = map(radians, location1[0], location1[1], location2[0], location2[1])
+	X1 = ( sin((latitude_2-latitude_1)/2.0) )**2
+	X2 = cos(latitude_1)*cos(latitude_2)*sin( ((longitude_2-longitude_1)/2.0)**2 )
+	d = 2*asin( (X1+X2)**0.5 )
+	return 1-d
+
+def cosine_similarity_score(vector1, vector2):
+	'''similarity between two tfidf score vectors of documents'''
+	return sklearn.metrics.pairwise.cosine_similarity( np.array(vector1).reshape(1,-1), np.array(vector2).reshape(1,-1) )[0][0]
--- SubEventDetection/sub_event.py
+++ SubEventDetection/sub_event.py
@@ -7,41 +7,19 @@ import csv
 import sklearn 
 import numpy as np
 import datetime
+import similarity_score as ss
 
-def tf_idf( doc, term, list_of_docs):
-	num_docs_containing_term = 0.0
-	for document in list_of_docs:
-		if term in document:
-			num_docs_containing_term += 1
+def cluster_date_similarity_score(cluster, point, feature_index):
+	''' return the similarity score of a cluster and a point with date as feature'''
+	return ss.date_similarity_score(cluster.centroid, point[feature_index])
 
-	if term in doc :
-		freq_term_in_doc = doc[term]
-	else :
-		freq_term_in_doc = 0
+def cluster_location_similarity_score(cluster, point, feature_index):
+	''' return the similarity score of a cluster and a point with location as feature'''
+	return ss.location_similarity_score(cluster.centroid, point[feature_index])
 
-	words_in_doc = 1.0 * sum(doc.values())
-	return (freq_term_in_doc / words_in_doc) * tf.log( len(list_of_docs) / num_docs_containing_term)
-
-def date_similarity_score(date1, date2):
-	''' if the difference in days is more than one month apart it is taken as 0'''
-	diff_in_days = abs(date1-date2)
-	seperation_limit_in_days = 30.0
-	if diff_in_days > seperation_limit_in_days:
-		return 0.0
-	return 1- ( diff_in_days / seperation_limit_in_days )
-
-def location_similarity_score(location1, location2):
-	''' similarity between two locations using the widely accepted Haversine Distance'''
-	longitude_1, latitude_1, longitude_2, latitude_2 = map(radians, location1[0], location1[1], location2[0], location2[1])
-	X1 = ( sin((latitude_2-latitude_1)/2.0) )**2
-	X2 = cos(latitude_1)*cos(latitude_2)*sin( ((longitude_2-longitude_1)/2.0)**2 )
-	d = 2*asin( (X1+X2)**0.5 )
-	return 1-d
-
-def cosine_similarity_score(vector1, vector2):
-	'''similarity between two tfidf score vectors of documents'''
-	return sklearn.metrics.pairwise.cosine_similarity( np.array(vector1).reshape(1,-1), np.array(vector2).reshape(1,-1) )[0][0]
-
+def cluster_text_similarity_score(cluster, point, feature_index):
+	''' return the similarity score of a cluster and a point with text as feature'''
+	return ss.cosine_similarity_score(cluster.centroid, point[feature_index])
 
 class Cluster:
 	'''class for a cluster'''
@@ -57,13 +35,13 @@ class Cluster:
 		self.centroid = doc[feature_index]
 
 	def similarity_score(self, doc):
-		return self.similarity_metric(self.centroid, doc[self.feature_index])
+		return self.similarity_metric(self, doc[self.feature_index])
 
 	def get_threshold():
 		return self.MBS*self.MBS_coeff
 
 	def average_similarity(self, doc_list):
-		return sum([self.similarity_metric(self.centroid, doc_list[index][self.feature_index]) for index in self.doc_list]) / float(len(self.doc_list))
+		return sum([self.similarity_metric(self, doc_list[index][self.feature_index]) for index in self.doc_list]) / float(len(self.doc_list))
 
 	def update_MBS(self, doc_list, doc_index):
 		if len(self.doc_list <= 25 ):
@@ -72,18 +50,13 @@ class Cluster:
 			self.MBS_prev = 0
 			self.MBS = average_similarity(doc_list)
 		else: 
-			self.MBS = (self.MBS * (len(self.doc_list) - 1) + self.similarity_metric(self.centroid, doc_list[doc_index][self.feature_index])) / float(len(self.doc_list))
+			self.MBS = (self.MBS * (len(self.doc_list) - 1) + self.similarity_metric(self, doc_list[doc_index][self.feature_index])) / float(len(self.doc_list))
 
 
 	def add_point(self, doc_list, doc_index):
 		self.doc_list.append(doc_index)
 		self.centroid = sum([ doc_list[index][self.feature_index] for index in self.doc_list])/len(self.doc_list)
 		self.update_MBS(doc_list, doc_index)
-
-
-
-
-
 
 class ClusterSolution:
 	'''class for cluster solution for a feature'''
